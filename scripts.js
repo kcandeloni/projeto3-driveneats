03baef0e6eeed60b50fc7b532a9f0adf349c7bab
kcandeloni <kcandeloni@gmail.com>
2022-06-12 21:17:07 -0300

    (Finalmente) Mensagem pelo Whats OK
    Tela de Confirmação de Pedido OK

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
+
+let pedidoWhats = "Olá, gostaria de fazer o pedido:2";
 function select_prato (elemento) {
     const remove_select = document.querySelector('.prato .select_item');
     if(remove_select !== null){
         remove_select.classList.remove("select_item");
     }
     elemento.classList.add("select_item");
-    confirma_pedido(1);
+    veifica_pedido(1);
 }
 
 function select_bebida (elemento) {
@@ -13,7 +15,7 @@ function select_bebida (elemento) {
         remove_select.classList.remove("select_item");
     }
     elemento.classList.add("select_item");
-    confirma_pedido(2);
+    veifica_pedido(2);
 }
 
 function select_cookie (elemento) {
@@ -22,14 +24,14 @@ function select_cookie (elemento) {
         remove_select.classList.remove("select_item");
     }
     elemento.classList.add("select_item");
-    confirma_pedido(3);
+    veifica_pedido(3);
 }
 
 let prato = false;
 let beb = false;
 let cookie = false;
 
-function confirma_pedido (n_ok) {
+function veifica_pedido (n_ok) {
     if(n_ok === 1){
         prato = true;
     }else if(n_ok === 2){
@@ -40,54 +42,81 @@ function confirma_pedido (n_ok) {
     if(prato && beb && cookie){
         let confirma = document.querySelector('.pedido button');
         confirma.classList.add("pedido_ok");
+        confirma.classList.add("colorButton");
         confirma.innerHTML = "<p>Fechar Pedido</p>";
     } 
 }
 
+function confirmaPedido (botao) {
+    if(botao.classList.contains("pedido_ok")){
+        let esconde_tela = document.querySelector(".transparent_tela");
+        esconde_tela.classList.toggle("escondido");
+        let pedido_tela = document.querySelector(".confirma");
+        pedido_tela.classList.toggle("escondido");
+        dadosPedido();
+    }
+}
 
-// function confere_pedido(caracter){
-//     let caracter_pedido = "";
-//     let prato = false;
-//     let bebida = false;
-//     let cookie = false;
-//     caracter_pedido = caracter_pedido + caracter;
-//     for(let i; i < caracter_pedido.length; i ++){
-//         if(caracter_pedido[i] === "p"){
-//             prato = true;
-//         }
-//         if(caracter_pedido[i] === "b"){
-//             bebida = true;
-//         }
-//         if(caracter_pedido[i] === "c"){
-//             cookie = true;
-//         }
-//     }
-//     if(prao && bebida && cookie){
-//         let elemento = document.querySelector('.pedido button');
-//         elemento.classList.add("pedido_ok");
-//         elemento.innerHTML("Fechar Pedido");
-//     }
+function dadosPedido () {
+    let selecionados = document.querySelector('.prato .select_item');
+    let getText = selecionados.querySelector('p1');
+    let nomePrato = getText.innerHTML;
+   
+    getText = selecionados.querySelector('p3');
+    let precoPrato = getText.innerHTML;
+    precoPrato = precoPrato.replace('R$ ','');
+    precoPrato = precoPrato.replace(',','.');
+    let n_precoPrato = Number(precoPrato);
 
+    selecionados = document.querySelector('.bebida .select_item');
+    getText = selecionados.querySelector('p1');
+    let nomeBeb = getText.innerHTML;
+    
+    getText = selecionados.querySelector('p3');
+    let precoBeb = getText.innerHTML;
+    precoBeb = precoBeb.replace('R$ ','');
+    precoBeb = precoBeb.replace(',','.');
+    let n_precoBeb = Number(precoBeb);
+
+    selecionados = document.querySelector('.cookie .select_item');
+    getText = selecionados.querySelector('p1');
+    let nomeCookie = getText.innerHTML;
+   
+    getText = selecionados.querySelector('p3');
+    let precoCookie = getText.innerHTML;
+    precoCookie = precoCookie.replace('R$ ','');
+    precoCookie = precoCookie.replace(',','.');
+    let n_precoCookie = Number(precoCookie);
+
+    let totalPedido = n_precoPrato + n_precoBeb + n_precoCookie;
+    pedidoWhats = `Olá, gostaria de fazer o pedido:
+- Prato: ${nomePrato}
+- Bebida: ${nomeBeb}
+- Sobremesa: ${nomeCookie}
+Total: R$ ${totalPedido.toFixed(2)}`;
+
+    let itensPedido = 
+    `<div><span>${nomePrato}</span><span>${precoPrato.replace('.',',')}</span></div>
+    <div><span>${nomeBeb}</span><span>${precoBeb.replace('.',',')}</span></div>
+    <div><span>${nomeCookie}</span><span>${precoCookie.replace('.',',')}</span></div>
+    <div><span><p>TOTAL:</p></span><span><p>R$ ${totalPedido.toFixed(2).replace('.',',')}</p></span></div>`;
+
+    let descricaoPedido = document.querySelector('p5');
+    descricaoPedido.innerHTML = itensPedido;
+}
+
+function enviaWhats () {
+    const nameUser = prompt("Informe seu nome: ");
+    const endUser = prompt("Informe seu Endereço: ");
+    pedidoWhats += `\n\nNome: ${nameUser}\nEndereço: ${endUser}`;
+    alert(pedidoWhats);
+    pedidoWhats = encodeURIComponent(pedidoWhats);
+    window.open(`https://wa.me/5555991191601?text=${pedidoWhats}`);
+}
+// function envia () {
+//     let elemPedido = document.querySelector(".pedido");
+
+//     alert(elemPedido);
 // }
-
-// function get_pedido {
-//     let name_prato = null;
-//     let price_praco = null;
-
-//     let name_bebida  = null;
-//     let price_bebida  = null;
-
-//     let name_sobremesa = null;
-//     let price_sobremesa = null;
-// }
-// function spec_pedido {
-//     let spec_quant = 0;
-
-//     if(spec_pedido == 3){
-//         libera_pedido();
-//     }
-// }
-
-// libera_pedido () {
-
-// }
+    /*const elemento = document.querySelector(".valor");
+        elemento.innerHTML = valorAtual;*/
